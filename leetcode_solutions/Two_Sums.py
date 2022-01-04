9b8a1ee239d699ad58d2e38cfdd8ad60f9358b2c
magicWiss <mat.wissel@stud.uniroma3.it>
2022-01-04 15:23:54 +0100

    Solution to Two Sum leet code
    Solution to leet code exercise 1 (Two Sums) using hashMap to store prev elements that were visited

---
+++ leetcode_solutions/Two_Sums.py
@@ -0,0 +1,27 @@
+'''
+Created on 4 gen 2022
+
+@author: Wissel
+
+Problem 1 of leet code: Two Sums
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+
+class Solution:
+    def twoSum(self, nums, target):
+        
+        prev_val={}                 #emptyHashmap
+        
+        for i,n in enumerate(nums):         #i is the index, n is the element
+            
+            current=target-n
+            
+            if current in prev_val:
+                return [i,prev_val[current]]
+            
+            prev_val[n]=i
+                
