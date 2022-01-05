da1e621d102ae5b2c4cfa353f80d67a89ba685ad
magicWiss <mat.wissel@stud.uniroma3.it>
2022-01-05 11:41:59 +0100

    median_of_two_sorted_arrays
    Merge di array e individuo il mediano in base alla lunghezza della lista creata.
    Se pari sommo len/2 e len/2-1 e li divido per due
    Se dispari invece è il valore intermedio a len/2

---
+++ leetcode_solutions/median_number_of_sorted_arrays.py
@@ -0,0 +1,24 @@
+'''
+Created on 5 gen 2022
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+@author: Wissel
+'''
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2) :
+        
+        total=nums1+nums2
+        output=0
+        total.sort()
+        
+        if(len(total)%2==0):            #pari
+            
+            output=total[len(total)//2]+total[len(total)//2+1]
+            output=output/2
+            return output
+        
+        return total[len(total)//2]
+        
+        
